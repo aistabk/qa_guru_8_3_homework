560138503627d4d38b3b27b3c5432be9e115bbbb
aistabk <elizavetasoom@ya.ru>
2023-09-24 11:53:39 +0300

    new test result

--- test_lesson_8_2.py
+++ test_lesson_8_2.py
@@ -4,7 +4,7 @@ from selene import browser, have, be
 def test_results_ok(browser_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Результатов: примерно 0'))
+    browser.element('[id="search"]').should(have.text('Результат поиска: yashaka/selene '))
 
 
 def test_results_not_ok(browser_size):
