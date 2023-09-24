bb0d1ef6dc2f988bf51e964b46db251b6d4e95f3
aistabk <elizavetasoom@ya.ru>
2023-09-24 11:48:05 +0300

    новый ОР, 1 тест

--- test_lesson_8_2.py
+++ test_lesson_8_2.py
@@ -4,7 +4,7 @@ from selene import browser, have, be
 def test_results_ok(browser_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Результаты поиска: yashaka/selene'))
+    browser.element('[id="search"]').should(have.text('Результатов: примерно 0'))
 
 
 def test_results_not_ok(browser_size):
